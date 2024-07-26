e2d9bcfc87f9f0395fa31cc8e8b84ec50ba24492
Prakash22-code <ratdeprakash@gmail.com>
2024-07-26 20:43:53 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,5 @@
+music = {
+        "made in romania" : "https://youtu.be/SJ8ppRj0Jcc?si=8ph7Ifo_vlMUp-X-",
+             "faasle"     : "https://youtu.be/9sekgEXGm-E?si=MuL16iTlAesqnA8U",
+             "skyfall"     : "https://youtu.be/DeumyOzKqgI?si=G1zeQgYs7eXIDQQN",
+}
